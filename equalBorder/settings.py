b9d2dacf8a77f151f1ce44397f4ccb0855061c7a
x.zhang <griffcho16@gmail.com>
2020-07-24 20:42:59 +0900

    changed settings.py to include default DB

--- equalBorder/settings.py
+++ equalBorder/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -83,9 +84,12 @@ DATABASES = {
         'USER': 'xzhang',
         'PASSWORD': '',
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': '',
     }
 }
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
