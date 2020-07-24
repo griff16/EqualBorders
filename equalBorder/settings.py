533f9a9a1ed645162a3205dd67dbe58a37d4f39f
x.zhang <griffcho16@gmail.com>
2020-07-24 18:19:18 +0900

    added postgres DB

--- equalBorder/settings.py
+++ equalBorder/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'equalBorder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'equalborders',
+        'USER': 'xzhang',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
