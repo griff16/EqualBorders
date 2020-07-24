8ce44bf01fcabcbc2296c3411bfc76b3d8cebc46
x.zhang <griffcho16@gmail.com>
2020-07-24 16:03:56 +0900

    added about, news, schools pages

--- summaryDashboard/urls.py
+++ summaryDashboard/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from summaryDashboard.views import summaryDashboardView
+from summaryDashboard.views import *
 
 urlpatterns = [
     path('', summaryDashboardView.as_view(), name='sumDash'),
+    path('about/', summaryDashboardAboutView.as_view(), name='sumDashAbout'),
+    path('schools/', summaryDashboardSchoolsView.as_view(), name='sumDashSchools'),
+    path('news/', summaryDashboardNewsView.as_view(), name='sumDashNews'),
 ]
--- summaryDashboard/views.py
+++ summaryDashboard/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
+
 class summaryDashboardView(TemplateView):
     template_name = "summaryDashboard.html"
+
+
+class summaryDashboardAboutView(TemplateView):
+    template_name = "about.html"
+
+
+class summaryDashboardSchoolsView(TemplateView):
+    template_name = "schools.html"
+
+
+class summaryDashboardNewsView(TemplateView):
+    template_name = "news.html"
