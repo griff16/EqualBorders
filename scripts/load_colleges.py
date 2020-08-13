b81036a1a82e60a70b7e80c60593cfe8b865b9e3
x.zhang <griffcho16@gmail.com>
2020-08-13 20:57:26 +0900

    added working progress bar and fixed db issue

--- scripts/load_colleges.py
+++ scripts/load_colleges.py
@@ -1,4 +1,4 @@
-import csv, os
+import csv, os, mmap
 from summaryDashboard.models import College
 from tqdm import tqdm
 
@@ -8,9 +8,14 @@ def run():
     College.objects.all().delete()
     
     with open(file_path) as f:
-        read_size = 0
-        with tqdm(total=file_size) as pbar:
-            for row in f:
-                obj, created = College.objects.get_or_create(code = row[0], name = row[1], link = row[2])
-                read_size += len(row.encode('utf-8'))
-                pbar.update(read_size)
+        reader = csv.reader(f, delimiter=',')
+        for row in tqdm(reader, total=get_num_lines(file_path)):
+            obj, created = College.objects.get_or_create(code = row[0], name = row[1], link = row[2])
+
+def get_num_lines(file_path):
+    fp = open(file_path, "r+")
+    buf = mmap.mmap(fp.fileno(), 0)
+    lines = 0
+    while buf.readline():
+        lines += 1
+    return lines
